3421c4a49f2d22b229f21fdcee8121453faf3e20
Kyle-Neale <kyle.a.neale@gmail.com>
2018-07-22 09:35:21 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+require "pry"
+
+class Anagram
+  attr_accessor :word
+  attr_reader :match
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |word_ele|
+      # binding.pry
+      word_ele.length && word_ele.split('').sort == (word.length && word.split('').sort)
+    end
+  end
+end
