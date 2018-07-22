ff03fa4d1d3c0ab0bca6b7c0e3ff6d866e06b619
personabe1984 <personabe1984@gmail.com>
2018-07-22 00:06:18 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+# Your code goes here! Ok!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word.scan(/\w/).sort
+  end
+
+  def match(word_array)
+    word_array.select { |word| word.scan(/\w/).sort == @word }
+  end
+
+end
